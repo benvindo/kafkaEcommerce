5c7865277d42fd31b74a4a61c344ebb22ad129ae
Bruno Costa <bruno.costa@compwire.com>
2020-05-11 15:24:51 -0300

    criação produtor

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-"# kafkaEcommerce" 
+# kafkaEcommerce
+
+### Comandos Kafka CDH 6 + Kerberos
+---
+* Criar o arquivo jaas.config
+
+```
+KafkaClient {
+ com.sun.security.auth.module.Krb5LoginModule required
+ useKeyTab=true
+ keyTab="/run/cloudera-scm-agent/process/1330-kafka-KAFKA_BROKER/kafka.keytab"
+ storeKey=true
+ useTicketCache=false
+ principal="kafka/ctn5.lab.local@KDC.CDH6";
+};
+```
+
+*Criar o arquivo consumer.properties
+```
+security.protocol=SASL_PLAINTEXT
+sasl.kerberos.service.name=kafka
+```
+
+* Exporta a variavel de ambiente
+>export KAFKA_OPTS="-Djava.security.auth.login.config=/tmp/jaas.config
+
+* Comandos
+
+###### _**Consumir Topico**_
+> kafka-console-consumer --bootstrap-server ctn2.lab.local:9092,ctn3.lab.local:9092,ctn4.lab.local:9092,ctn5.lab.local:9092 --topic teste --from-beginning --consumer.config consumer.propertiers
+
+###### _**Produzir Topico**_
+> kafka-console-consumer --bootstrap-server ctn2.lab.local:9092 --topic teste --from-beginning --consumer.config consumer.propertiers
+
+###### _**Lista Topicos**_
+>kafka-topics --list --zookeeper ctn1.lab.local,ctn2.lab.local,ctn3.lab.local:2181
+
+###### _**Descrever Topico**_
+>kafka-topics --describe --zookeeper ctn1.lab.local,ctn2.lab.local,ctn3.lab.local:2181 --topic teste
---
+++ src/main/java/NewOrderMain.java
@@ -0,0 +1,38 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.zookeeper.util.SecurityUtils;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "123424,65283,92635182";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) -> {
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " +
+                    data.offset() + "/ timestamp" + data.timestamp());
+        }).get();
+    }
+
+    public static Properties properties(){
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "ctn2.lab.local:9092,ctn3.lab.local:9092,ctn4.lab.local:9092,ctn5.lab.local:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "1");
+        properties.setProperty("security.protocol", "SASL_PLAINTEXT");
+        properties.setProperty("sasl.kerberos.service.name", "kafka");
+        return properties;
+
+    }
+
+}
