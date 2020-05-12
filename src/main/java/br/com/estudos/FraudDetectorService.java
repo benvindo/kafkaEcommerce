e88e3616b00f7f3374d2ce03ea489bff943c66f8
Bruno Costa <bruno.costa@compwire.com>
2020-05-12 15:02:12 -0300

    max-pool

--- src/main/java/br/com/estudos/FraudDetectorService.java
+++ src/main/java/br/com/estudos/FraudDetectorService.java
@@ -49,6 +49,7 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
         properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"1");
         properties.setProperty("security.protocol", "SASL_PLAINTEXT");
         properties.setProperty("sasl.kerberos.service.name", "kafka");
         return properties;
