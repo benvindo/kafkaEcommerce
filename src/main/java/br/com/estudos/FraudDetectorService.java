b7fa1ce4b543ba9652a0e45b79492732f2b28d9a
Bruno Costa <bruno.costa@compwire.com>
2020-05-13 19:14:27 -0300

    camada consumidor

--- src/main/java/br/com/estudos/FraudDetectorService.java
+++ src/main/java/br/com/estudos/FraudDetectorService.java
@@ -1,6 +1,7 @@
 package br.com.estudos;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.log4j.Level;
@@ -16,44 +17,26 @@ import java.util.UUID;
 public class FraudDetectorService {
 
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-
-        while(true){
-            var records = consumer.poll(Duration.ofMillis(100));
-            if(!records.isEmpty()){
-                System.out.println("Encontrei " + records.count() + " registros");
-                for (var record : records) {
-                    System.out.println("------------------------------------------");
-                    System.out.println("Processing new order, checking for fraud");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        // ignoring
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order processed");
-                }
-            }
-        }
+        FraudDetectorService fraudService = new FraudDetectorService();
+        var service = new KafkaService(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER",
+                fraudService::parse);
+        service.run();
     }
 
-    public static Properties properties(){
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "ctn2.lab.local:9092,ctn3.lab.local:9092,ctn4.lab.local:9092,ctn5.lab.local:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"1");
-        properties.setProperty("security.protocol", "SASL_PLAINTEXT");
-        properties.setProperty("sasl.kerberos.service.name", "kafka");
-        return properties;
-
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Order processed");
     }
 
 
