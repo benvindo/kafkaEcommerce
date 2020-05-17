82178579762767128745e7f607ccf574ff887fed
Bruno Costa <bruno.costa@compwire.com>
2020-05-17 11:50:18 -0300

    produtores consumidores

--- service-frauddetector/src/main/java/br/com/estudos/Order.java
+++ service-frauddetector/src/main/java/br/com/estudos/Order.java
@@ -12,5 +12,20 @@ public class Order {
         this.amount = amount;
     }
 
+    public String getUserId() {
+        return userId;
+    }
 
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "userId='" + userId + '\'' +
+                ", orderId='" + orderId + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
