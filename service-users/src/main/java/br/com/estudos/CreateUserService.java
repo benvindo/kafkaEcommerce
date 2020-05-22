16319b96195859059cee7da3b7e04dd3e5412736
Bruno Costa <bruno.costa@compwire.com>
2020-05-22 17:39:36 -0300

    serviço externos

---
+++ service-users/src/main/java/br/com/estudos/CreateUserService.java
@@ -0,0 +1,64 @@
+package br.com.estudos;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class CreateUserService {
+
+    private final Connection connection;
+
+    CreateUserService () throws SQLException {
+        String url = "jdbs:target/sqlite:users_database.db";
+        this.connection = DriverManager.getConnection(url);
+        connection.createStatement().execute("create table Users (" +
+                "uuid varchar(200) primary key," +
+                "email varchar(200))");
+    }
+
+    public static void main(String[] args) throws SQLException {
+        CreateUserService createUserService = new CreateUserService();
+        try (var service = new KafkaService<>(CreateUserService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                createUserService::parse, Order.class,
+                Map.of()
+        )) {
+            service.run();
+        }
+    }
+
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
+
+    private void parse(ConsumerRecord<String, Order> record) throws SQLException {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking new user");
+        System.out.println(record.value());
+
+        var order = record.value();
+        if(isNewUser(order.getEmail())){
+            insertNewUser(order.getEmail());
+        }
+
+    }
+
+    private void insertNewUser(String email) throws SQLException {
+        var insert = connection.prepareStatement("insert into Users (uuid, email) values (?, ?)");
+        insert.setString( 1, "uuuid");
+        insert.setString( 2, email);
+        System.out.println("Usuario uuid" + email + "adicionado");
+    }
+
+    private boolean isNewUser(String email) throws SQLException {
+        var exists = connection.prepareStatement("select uuid from Users where email = ? limit 1");
+        exists.setString(1, email);
+        var results = exists.executeQuery();
+        return !results.next();
+    }
+
+
+}
